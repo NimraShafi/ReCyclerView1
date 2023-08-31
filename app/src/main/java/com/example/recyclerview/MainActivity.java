ca1234147e8a9323e94a17f665f0fe6c9ff5b5d7
lab2 <lab2@BF02LAB-PC06.ssuet.edu.pk>
2023-08-31 19:47:33 +0500

    Initial commit

---
+++ app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.recyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView view;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        view = (RecyclerView) findViewById(R.id.reCyclerView);
+    }
+}
